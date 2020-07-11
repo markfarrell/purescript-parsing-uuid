9db803c9520b5c71215b57af4e28d11f8c6eec6a
Mark Farrell <markfarr2011@gmail.com>
2020-07-11 22:56:55 +0000

    lowercase, uppercase, any

--- src/Text/Parsing/String/UUID.purs
+++ src/Text/Parsing/String/UUID.purs
@@ -1,5 +1,7 @@
-module Text.Parsing.String.UUID 
-  ( any
+module Text.Parsing.String.UUID
+  ( lowercase
+  , uppercase
+  , any
   ) where
 
 import Prelude
@@ -14,19 +16,57 @@ import Text.Parsing.String.Repetition as R
 
 -- | Consumes a valid UUID string (xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx), or fails otherwise.
 -- | Does not validate UUID version `M` and UUID variant of `N`.
--- | Fails if the 5th group is followed by additional lowercase hexadecimal characters. 
-any :: forall a m. S.StringLike a => Monad m => ParserT a m String
-any = do
-  v <- R.least 8  $ H.lowercase 
+-- | Fails if the 5th group is followed by additional lowercase hexadecimal characters.
+lowercase :: forall a m. S.StringLike a => Monad m => ParserT a m String
+lowercase = do
+  v <- R.exact 8  $ H.lowercase
   _ <- hyphen
-  w <- R.least 4  $ H.lowercase
+  w <- R.exact 4  $ H.lowercase
   _ <- hyphen
-  x <- R.least 4  $ H.lowercase
+  x <- R.exact 4  $ H.lowercase
   _ <- hyphen
-  y <- R.least 4  $ H.lowercase
+  y <- R.exact 4  $ H.lowercase
   _ <- hyphen
   z <- R.exact 12 $ H.lowercase
   pure $ F.intercalate delimiter [v,w,x,y,z]
-  where 
+  where
     hyphen    = S.char '-'
-    delimiter = "-" 
+    delimiter = "-"
+
+-- | Consumes a valid UUID string (xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx), or fails otherwise.
+-- | Does not validate UUID version `M` and UUID variant of `N`.
+-- | Fails if the 5th group is followed by additional uppercase hexadecimal characters.
+uppercase :: forall a m. S.StringLike a => Monad m => ParserT a m String
+uppercase = do
+  v <- R.exact 8  $ H.uppercase
+  _ <- hyphen
+  w <- R.exact 4  $ H.uppercase
+  _ <- hyphen
+  x <- R.exact 4  $ H.uppercase
+  _ <- hyphen
+  y <- R.exact 4  $ H.uppercase
+  _ <- hyphen
+  z <- R.exact 12 $ H.uppercase
+  pure $ F.intercalate delimiter [v,w,x,y,z]
+  where
+    hyphen    = S.char '-'
+    delimiter = "-"
+
+-- | Consumes a valid UUID string (xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx), or fails otherwise.
+-- | Does not validate UUID version `M` and UUID variant of `N`.
+-- | Fails if the 5th group is followed by additional hexadecimal characters.
+any :: forall a m. S.StringLike a => Monad m => ParserT a m String
+any = do
+  v <- R.exact 8  $ H.any
+  _ <- hyphen
+  w <- R.exact 4  $ H.any
+  _ <- hyphen
+  x <- R.exact 4  $ H.any
+  _ <- hyphen
+  y <- R.exact 4  $ H.any
+  _ <- hyphen
+  z <- R.exact 12 $ H.any
+  pure $ F.intercalate delimiter [v,w,x,y,z]
+  where
+    hyphen    = S.char '-'
+    delimiter = "-"
--- test/Text/Parsing/String/UUID.purs
+++ test/Text/Parsing/String/UUID.purs
@@ -10,18 +10,63 @@ import Text.Parsing.String.UUID as UUID
 
 import Text.Parsing.Expect as E
 
-any :: Effect Unit
-any = do
+lowercase :: Effect Unit
+lowercase = do
   _ <- failure ""
   _ <- failure "82fa951b"
+  _ <- failure "82fa951bb"
   _ <- failure "82fa951b-b591"
   _ <- failure "82fa951b-b591-4ace"
   _ <- failure "82fa951b-b591-4ace-b473"
   _ <- failure "82fa951b-b591-4ace-b473-f2aacaed56f"
   _ <- success "82fa951b-b591-4ace-b473-f2aacaed56fd"
+  _ <- failure "82fa951bb-b591-4ace-b473-f2aacaed56fd"
   _ <- failure "82fa951b-b591-4ace-b473-f2aacaed56fda"
+  _ <- failure "82fa951b-b591-4ace-b473-f2aacaed56fdaa"
   _ <- output  "82fa951b-b591-4ace-b473-f2aacaed56fd" "82fa951b-b591-4ace-b473-f2aacaed56fd"
   _ <- output  "123e4567-e89b-12d3-a456-426614174000" "123e4567-e89b-12d3-a456-426614174000"
+  pure unit
+  where
+    success = \check -> E.success check (UUID.lowercase)
+    failure = \check -> E.failure check (UUID.lowercase)
+    output  = \check expect -> E.output check expect (UUID.lowercase)
+
+uppercase :: Effect Unit
+uppercase = do
+  _ <- failure ""
+  _ <- failure "82FA951B"
+  _ <- failure "82FA951BB"
+  _ <- failure "82FA951B-B591"
+  _ <- failure "82FA951B-B591-4ACE"
+  _ <- failure "82FA951B-B591-4ACE-B473"
+  _ <- failure "82FA951B-B591-4ACE-B473-F2AACAED56F"
+  _ <- success "82FA951B-B591-4ACE-B473-F2AACAED56FD"
+  _ <- failure "82FA951BB-B591-4ACE-B473-F2AACAED56FD"
+  _ <- failure "82FA951B-B591-4ACE-B473-F2AACAED56FDA"
+  _ <- failure "82FA951B-B591-4ACE-B473-F2AACAED56FDAA"
+  _ <- output  "82FA951B-B591-4ACE-B473-F2AACAED56FD" "82FA951B-B591-4ACE-B473-F2AACAED56FD"
+  _ <- output  "123E4567-E89B-12D3-A456-426614174000" "123E4567-E89B-12D3-A456-426614174000"
+  pure unit
+  where
+    success = \check -> E.success check (UUID.uppercase)
+    failure = \check -> E.failure check (UUID.uppercase)
+    output  = \check expect -> E.output check expect (UUID.uppercase)
+
+any :: Effect Unit
+any = do
+  _ <- failure ""
+  _ <- failure "82Fa951b"
+  _ <- failure "82Fa951BB"
+  _ <- failure "82FA951b-B591"
+  _ <- failure "82FA951B-B591-4ACe"
+  _ <- failure "82FA951B-B591-4ACE-B473"
+  _ <- failure "82FA951B-B591-4ACE-B473-F2AACAED56F"
+  _ <- success "82FA951B-B591-4Ace-b473-F2AaCAed56FD"
+  _ <- failure "82FA951BB-B591-4ACE-B473-F2AACAED56FD"
+  _ <- failure "82FA951B-B591-4ACE-B473-F2AACAED56FDA"
+  _ <- failure "82FA951B-B591-4ACe-B473-F2AACAED56FDAA"
+  _ <- output  "82FA951b-b591-4ACE-B473-F2AACAED56FD" "82FA951b-b591-4ACE-B473-F2AACAED56FD"
+  _ <- output  "123e4567-E89B-12D3-A456-426614174000" "123e4567-E89B-12D3-A456-426614174000"
   pure unit
   where
     success = \check -> E.success check (UUID.any)
@@ -30,5 +75,7 @@ any = do
 
 main :: Effect Unit
 main = do
+  _ <- lowercase
+  _ <- uppercase
   _ <- any
   pure unit
